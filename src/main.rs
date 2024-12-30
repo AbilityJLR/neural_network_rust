f143b64c0a8a15c3a37685ce31d80eb87733d184
Tanakrit Maenphol <Thanagrith@gmail.com>
2024-12-30 12:29:41 +0700

    update restruct code and make test case

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-fn predict(w: &f32, x: &f32) -> f32 {
-    w*x
+fn predict(w: &mut f32, inputs: &[f32; 4]) -> [f32; 4]{
+    let mut pred: [f32; 4] = [0.0; 4];
+    for (i, input) in inputs.iter().enumerate() {
+        pred[i] = *w * input;
+    }
+    pred
 }
 
 fn main() {
@@ -10,10 +14,7 @@ fn main() {
     let targets: [f32; 4] = [2.0,4.0,6.0,8.0];
 
     for _ in 1..=25{
-        let mut pred: [f32; 4] = [0.0; 4];
-        for (i, &input) in inputs.iter().enumerate() {
-            pred[i] = predict(&w, &input);
-        }
+        let pred = predict(&mut w, &inputs);
         
         let mut errors: [f32; 4] = [0.0; 4];
         for (i, (t, p)) in targets.iter().zip(pred.iter()).enumerate() {
@@ -21,7 +22,15 @@ fn main() {
         }
 
         let mut cost: f32 = errors.iter().sum::<f32>() / targets.len() as f32;
-        println!("Weight: {:?}, Cost: {:?}", w, cost);
+        println!("Weight: {:.4}, Cost: {:.4}", w, cost);
         w += learning_rate * cost;
+    }
+
+    let test_inputs = [5.0, 6.0, 7.0, 8.0];
+    let test_targets = [10.0, 12.0, 14.0, 16.0];
+
+    let pred = predict(&mut w, &test_inputs);
+    for ((i, t), p) in test_inputs.iter().zip(test_targets.iter()).zip(pred.iter()) {
+        println!("Input: {:.4}, Target: {:.4}, Pred: {:.4}", i, t, p);
     }
 }
